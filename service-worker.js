4bbf8a9e28019317110e3f3abe255363fb4e435e
Black Pete <blackpete@goldenbay.ai>
2025-10-15 13:34:08 +1300

    Final fix: Clean up UI, remove test code, add network-first service worker

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+﻿// Service Worker for Automationator PWA - Network First Strategy
+const CACHE_NAME = 'automationator-v5';
+const urlsToCache = [
+  './manifest.webmanifest',
+  './images/icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  
+  if (event.request.headers.get('accept').includes('text/html') || url.pathname.endsWith('.html') || url.pathname === '/' || url.pathname === '/index.html') {
+    event.respondWith(
+      fetch(event.request).then((response) => {
+        return response;
+      }).catch(() => {
+        return caches.match(event.request);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
